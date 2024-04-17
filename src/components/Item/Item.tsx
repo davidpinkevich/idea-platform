b49ba5ca0fc88c6b1c1caf6ccd0f7a7bc0abcd6f
davidpinkevich <davidpinkevich95@gmail.com>
2024-04-17 19:26:31 +0300

    feat: add item

---
+++ src/components/Item/Item.tsx
@@ -0,0 +1,28 @@
+import { useState, memo } from "react";
+import { Paper } from "@mui/material";
+import { ButtonBuying } from "./ButtonBuying/ButtonBuying";
+import { ItemInfo } from "./ItemInfo/ItemInfo";
+import { TypeItem } from "types";
+import styles from "./styles.module.scss";
+
+const Item = memo(({ view, currency, rate }: TypeItem) => {
+  const [hover, setHover] = useState<boolean>(false);
+  const handlerButton = (value: boolean) => setHover(!value);
+
+  return (
+    <Paper elevation={hover ? 8 : 4}>
+      <div className={styles.item}>
+        <ButtonBuying
+          hover={hover}
+          changeHover={handlerButton}
+          currency={currency}
+          price={view.price}
+          rate={rate}
+        />
+        <ItemInfo info={view} hover={hover} />
+      </div>
+    </Paper>
+  );
+});
+
+export { Item };
